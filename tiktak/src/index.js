3d169d70497d40b7e5da549cbb310222ae7955c8
Oscar Nunez <noscar07@gmail.com>
2021-02-02 23:09:26 -0500

    completed game

--- tiktak/src/index.js
+++ tiktak/src/index.js
@@ -36,6 +36,9 @@ function Square (props) {
 
     handleClick(i) {
       const squares= this.state.squares.slice();
+      if (calculateWinner(squares) || squares[i]){
+        return;
+      }
       squares[i] = this.state.xIsnext ? 'X' : 'O';
       this.setState({
         squares: squares,
@@ -50,7 +53,13 @@ function Square (props) {
     }
   
     render() {
-      const status = 'Next player: X';
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if(winner){
+        status= "You Won! " + winner;
+      } else {
+        status= "Next Player: " + (this.state.xIsnext ? 'X' : 'O');
+      }
   
       return (
         <div>
@@ -98,3 +107,22 @@ function Square (props) {
     document.getElementById('root')
   );
   
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
